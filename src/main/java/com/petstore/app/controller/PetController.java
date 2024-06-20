967eeef1d3349c526232ce0254cea87ccb114ca1
itfyahisa <yahisa.naoya@itfllc.co.jp>
2024-06-20 17:07:51 +0900

    URL修正

--- src/main/java/com/petstore/app/controller/PetController.java
+++ src/main/java/com/petstore/app/controller/PetController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/pets")
+@RequestMapping("/pet")
 public class PetController {
 
 	private final PetService petService;
