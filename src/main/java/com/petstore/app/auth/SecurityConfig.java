0b1e5ec369f87b208e36c8e36871e2e32f556567
itfyahisa <yahisa.naoya@itfllc.co.jp>
2024-06-20 17:06:04 +0900

    認証無効化

--- src/main/java/com/petstore/app/auth/SecurityConfig.java
+++ src/main/java/com/petstore/app/auth/SecurityConfig.java
@@ -1,26 +1,26 @@
-package com.petstore.app.auth;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-	
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		http
-			.csrf(Customizer.withDefaults())
-			.authorizeHttpRequests(authorize -> authorize
-//					.requestMatchers("pets").permitAll()
-					.anyRequest().authenticated()
-			)
-			.httpBasic(Customizer.withDefaults());
-		return http.build();
-	}
-	
-}
+//package com.petstore.app.auth;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//	
+//	@Bean
+//	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//		http
+//			.csrf(Customizer.withDefaults())
+//			.authorizeHttpRequests(authorize -> authorize
+//					.anyRequest().permitAll()
+////					.anyRequest().authenticated()
+//			);
+////			.httpBasic(Customizer.withDefaults());
+//		return http.build();
+//	}
+//	
+//}
--- src/main/java/com/petstore/app/auth/UserConfig.java
+++ src/main/java/com/petstore/app/auth/UserConfig.java
@@ -1,27 +1,27 @@
-package com.petstore.app.auth;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-public class UserConfig {
-	
-	@Bean
-	InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.withUsername("user")
-				.password(passwordEncoder().encode("password"))
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-}
+//package com.petstore.app.auth;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.core.userdetails.*;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//public class UserConfig {
+//	
+//	@Bean
+//	InMemoryUserDetailsManager userDetailsService() {
+//		UserDetails user = User.withUsername("user")
+//				.password(passwordEncoder().encode("password"))
+//				.roles("USER")
+//				.build();
+//		return new InMemoryUserDetailsManager(user);
+//	}
+//	
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//}
